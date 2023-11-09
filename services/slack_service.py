4be58c91d04f3dbf3e921d755c171aa391d4ac79
martin-nhs <martin.taylor22@nhs.net>
2023-11-09 15:17:37 +0000

    Small logic modifications to slack_service.py

--- services/slack_service.py
+++ services/slack_service.py
@@ -1,22 +1,19 @@
 import os
 
-from loguru import logger
+from utilities.aws_utilities import fetch_ssm_parameter
+from models.structured_event import StructuredEvent
 from urllib3 import request, BaseHTTPResponse
 from .template_service import TemplateService
+from loguru import logger
 
 
 class SlackService:
     def __init__(self):
-        self._slack_webhook = os.environ["SLACK_WEBHOOK_URL"]
+        self._slack_webhook = fetch_ssm_parameter(os.environ["SLACK_WEBHOOK_URL_SSM_PARAMETER"], True)
         self._template_service = TemplateService()
 
-    def send_notification(self, message: str, nhs_environment: str, service_name: str):
-        templated_message: str = self._template_service.get_slack_notification_template(
-            message,
-            nhs_environment,
-            service_name
-        )
-
+    def send_notification(self, event: StructuredEvent):
+        templated_message: str = self._template_service.get_slack_notification_template(event)
         response: BaseHTTPResponse = request(method="POST",
                                              url=self._slack_webhook,
                                              body=templated_message,
@@ -24,3 +21,5 @@ class SlackService:
 
         if response.status != 200:
             logger.error(f"The message failed to send to Slack with HTTP status {response.status}.")
+        else:
+            logger.info("The message has successfully been sent to Slack.")
